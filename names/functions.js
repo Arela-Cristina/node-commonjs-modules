7a1ecfd48efb71a633b966d5ab9edaebadcb238f
Arela-Cristina <instagrandine@gmail.com>
2024-11-15 15:21:51 +0100

    create function, export whit js Modules

---
+++ names/functions.js
@@ -0,0 +1,4 @@
+//Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+export function fullName(firstName, LastName) { //export per importare con JS Modules
+  return firstName + ' ' + LastName;
+}
